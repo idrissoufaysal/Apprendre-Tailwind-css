8bd9966a4e9c9e6c631ff87637da12fb0b475656
idrissou <idrissoufaysal93@gmail.com>
2024-05-02 23:48:49 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-
 function App() {
-
   return (
-    <div className="flex flex-col gap-4 items-center justify-center">
-      <button className='h-50 w-100 bg-red-700 font-bold text-gray-500 p-2'>bonjour </button>
-      <button className='h-50 w-20 bg-green-200 font-bold rounded-lg text-cyan-400'>bonjour </button>
-
+    <div className="w-full h-full  flex flex-col items-center justify-center gap-5 ">
+      <div className="h-40 w-100 bg-gray-300 rounded-2 xl p-5 mt-40 flex gap-3">
+        <button className="h-10 w-20 bg-red-400 font-bold rounded-md text-gray-500 p-2 item-center text-black">
+          bonjour{" "}
+        </button>
+        <button className="h-10 w-20 bg-green-300 font-bold rounded-lg text-cyan-400 text-blue-700">
+          bonjour
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
